50783751ea14c07d5f70f4b2134b027e52654845
freshtehAhmadi <freshtehahmadi79@gmail.com>
2022-06-21 16:52:56 +0430

    paryment

--- loan/urls.py
+++ loan/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', include('books.urls')),
     path('add_loan/<int:pk>', views.add_loan, name="add_loan"),
     path('loan/', views.loan_list, name="loan"),
+    path('payment/', views.payment, name="payment"),
+    path('done_payment/', views.done_payment, name="done_payment"),
     path('check_receive/', views.check_receive, name="check_receive"),
     path('user_loan/<int:pk>', views.user_loan, name="user_loan"),
     path('receive/<int:pk>', views.receive, name="receive"),
--- loan/views.py
+++ loan/views.py
@@ -12,12 +12,15 @@ from extra.models import Comment, LikeBook, LikeComment, BookMarck
 from loan.models import LoanModel, DebtModel
 
 
-
+@login_required(login_url='login')
 def add_loan(request, pk):
     book = Book.objects.get(id=pk)
     try:
         user = CustomUserModel.objects.get(user=request.user)
-        valid = LoanModel.objects.filter(user=user, status='S').count()
+        start = LoanModel.objects.filter(user=user, status='S')
+        to_be_returned = LoanModel.objects.filter(user=user, status='T')
+        sum = start | to_be_returned
+        valid = sum.count()
         if valid < 5:
             loan = LoanModel.objects.create(user=user,book=book, status='S')
             messages.success(request, 'Your request has been successfully submitted...')
@@ -28,15 +31,43 @@ def add_loan(request, pk):
     return redirect('detail', pk=book.id)
 
 
+@login_required(login_url='login')
 def loan_list(request):
     try:
         user = CustomUserModel.objects.get(user=request.user)
+        start = LoanModel.objects.filter(user=user, status='S')
+        to_be_returned = LoanModel.objects.filter(user=user, status='T')
+        loan = start | to_be_returned
         content = {
-            'loan': LoanModel.objects.filter(user=user, status='S'),
+            'loan': loan,
         }
     except:
         messages.error(request, 'Please complete your user information!!')
     return render(request, 'loan/loan_list.html', content)
+
+
+@login_required(login_url='login')
+def done_payment(request):
+    custom_user = CustomUserModel.objects.get(user=request.user)
+    debt = DebtModel.objects.filter(user=custom_user)
+    if debt.exists():
+        for obj in debt:
+            obj.amount = 0
+            obj.save()
+        return redirect('loan')
+    else:
+        return redirect('loan')
+
+
+
+@login_required(login_url='login')
+def payment(request):
+    return render(request, 'loan/payment.html')
+
+
+
+    
+
 
 
 @login_required(login_url='login')
