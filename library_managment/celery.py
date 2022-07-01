cdaece5ddebe30239b769e79fbb48be1f2fa3c14
freshtehAhmadi <freshtehahmadi79@gmail.com>
2022-07-01 16:14:53 +0430

    compelet celery

--- library_managment/celery.py
+++ library_managment/celery.py
@@ -24,8 +24,9 @@ from datetime import datetime
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
+    # Execute daily at midnight...
     sender.add_periodic_task(
-        crontab(),
+        crontab(minute=0, hour=0),
         test(),
     )
 
@@ -34,103 +35,22 @@ def test():
     loan = LoanModel.objects.all()
     for obj in loan:
         if obj.status == 'S':
-            diff = datetime.now() - obj.start_date
-            if diff.days >= 1:
+            diff = datetime.now().date() - obj.start_date.date()
+            if diff.days >= 30:
                 obj.status = 'T'
                 obj.save()
-                # expiration = diff.days - 30
+                expiration = diff.days - 30
                 
-                # a = int(expiration) / 7
-                a = int(diff.days) / 2
+                a = int(expiration) / 7
                 
                 debt = DebtModel.objects.create(loan=obj, book=obj.book, user=obj.user, amount=a*2000)
                 
         elif obj.status == 'T':
-            diff = datetime.now() - obj.start_date
-            if diff.days >= 1:
-                # expiration = diff.days - 30
-                # a = int(expiration) / 7
-                a = int(diff.days) / 2
+            diff = datetime.now().date() - obj.start_date.date()
+            if diff.days >= 30:
+                expiration = diff.days - 30
+                a = int(expiration) / 7
                 debt = DebtModel.objects.get(loan=obj)
                 debt.amount = a * 2000
                 debt.save()
     return 'done!!'
-
-
-# ------------------------------------------------------------------
-# from celery import Celery
-# import os
-# import django
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library_managment.settings')
-# django.setup()
-
-# BROKER_URL = "amqp://guest:guest@localhost:5672/"
-
-# app = Celery(
-# 	'library_managment', 
-# 	broker=BROKER_URL,
-# )
-# app.autodiscover_tasks(packages=['tasks'])
-
-# app.config_from_object(celeryconfig)
-
-# app.config_from_object('library_managment.settings')
-
-# if __name__ == '__main__':
-# 	app.start()
-
-# celery -A library_managment worker -l info
-# --------------------------------------------------------------------------
-# from __future__ import absolute_import, unicode_literals
-# from celery.task import task
-# import os
-# from celery import Celery
-# import celery
-# from celery.schedules import crontab
-# import django
-# import os
-# from library_managment import settings as celeryconf
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library_managment.settings')
-# django.setup()
-
-# BROKER_URL = "pyamqp://guest:guest@localhost/"
-
-# app = Celery('library_managment', broker=BROKER_URL)
-
-# app.config_from_object('library_managment.settings')
-
-
-# app.autodiscover_tasks()
-
-# CELERY_IMPORTS = (
-#     'loan.tasks.expiration',
-# )
-
-# app.conf.beat_schedule = {
-#     # Executes every day at  00:00.
-#     'run-every-midnight': {
-#         'task': 'tasks.expiration',
-#         'schedule': crontab(hour=0, minute=0),
-#         'args': (),
-#     },
-# }
-
-
-# @app.on_after_configure.connect
-# def setup_periodic_tasks(sender, **kwargs):
-#     sender.add_periodic_task(
-#         crontab(),
-#         test(),
-#     )
-    
-# app.conf.timezone = 'Asia/Tehran'
-# from books.models import Author
-
-# @app.task
-# def test():
-# 	Author.objects.create(name='very_test', description='very_test')
-# 	return 1
-# --------------------------------------------------------------------
-
--- library_managment/settings.py
+++ library_managment/settings.py
@@ -87,16 +87,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'library_managment.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
  'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -152,6 +142,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 # SMTP configuration
 
