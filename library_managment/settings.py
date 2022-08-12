f2730303f453345e895a5560ca0bf37306ece208
Fereshteh <freshtehahmadi79@gmail.com>
2022-08-12 22:31:27 +0430

    Update settings.py

--- library_managment/settings.py
+++ library_managment/settings.py
@@ -156,8 +156,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'librarymanagement.django@gmail.com'
-EMAIL_HOST_PASSWORD ='govzgjrwjsdzkgdv'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD =''
 EMAIL_USE_TLS = True
 
 # Activate Django-Heroku.
